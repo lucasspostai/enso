4620b51b90c25fb31a5f69709083addef590ab90
Lucas Schneider Postai <lucasspostai@live.com>
2019-09-08 15:42:26 -0300

    feat: add player input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public static Vector2 Movement;
+
+    private void Update()
+    {
+        Movement.x = Input.GetAxis("Horizontal");
+        Movement.y = Input.GetAxis("Vertical");
+    }
+}
