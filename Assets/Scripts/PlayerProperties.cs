a18ea6ea7267b2f019aec265da7cd6171189a07a
Lucas Schneider Postai <lucasspostai@live.com>
2019-09-08 15:48:40 -0300

    feat: add player properties scriptable object

---
+++ Assets/Scripts/PlayerProperties.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerProperties", menuName = "Enso/Player")]
+public class PlayerProperties : ScriptableObject
+{
+    public float MoveSpeed;
+}
