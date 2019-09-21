91e9d5439db4487e758293f26637886671e6426e
Lucas Schneider Postai <lucasspostai@live.com>
2019-09-20 21:20:38 -0300

    feat: add camera follow to the player

---
+++ Assets/Scripts/CameraProperties.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CameraProperties", menuName = "Enso/Camera")]
+public class CameraProperties : ScriptableObject
+{
+    public float VerticalOffset;
+    public float LookAheadDistance;
+    public float LookAheadSmoothTime;
+    public Vector2 FocusAreaSize;
+}
