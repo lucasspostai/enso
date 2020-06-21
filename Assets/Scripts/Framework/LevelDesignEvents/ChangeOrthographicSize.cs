6219e36ecaac4bcaf48fd9755c2c0aca2444ae3c
Lucas Postai <lucasspostai@live.com>
2020-06-21 09:54:36 -0300

    fix: remove Test Pool

---
+++ Assets/Scripts/Framework/LevelDesignEvents/ChangeOrthographicSize.cs
@@ -0,0 +1,17 @@
+﻿using Enso.Characters.Player;
+using UnityEngine;
+
+namespace Framework.LevelDesignEvents
+{
+    public class ChangeOrthographicSize : LevelDesignEvent
+    {
+        [SerializeField] private float OrthographicSize;
+        
+        public override void Execute()
+        {
+            base.Execute();
+
+            FindObjectOfType<PlayerCinemachineManager>().VirtualCamera.m_Lens.OrthographicSize = OrthographicSize;
+        }
+    }
+}
