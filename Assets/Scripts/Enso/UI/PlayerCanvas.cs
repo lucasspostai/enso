1ce8d7672ba8dfe38de6c83c8fe5d580797f9bbc
Lucas Postai <lucasspostai@live.com>
2020-06-26 22:46:13 -0300

    feat: add death screen

--- Assets/Scripts/Enso/UI/PlayerCanvas.cs
+++ Assets/Scripts/Enso/UI/PlayerCanvas.cs
@@ -1,9 +1,36 @@
-﻿using Framework;
+﻿using Enso.Characters.Player;
+using Framework;
+using UnityEngine;
 
 namespace Enso.UI
 {
     public class PlayerCanvas : Singleton<PlayerCanvas>
     {
-    
+        private Player player;
+
+        [SerializeField] private GameObject DeathScreen;
+        
+        private void OnEnable()
+        {
+            player = FindObjectOfType<Player>();
+
+            if (player == null)
+                return;
+
+            player.GetHealthSystem().Death += EnableDeathScreen;
+        }
+
+        private void OnDisable()
+        {
+            if (player == null)
+                return;
+
+            player.GetHealthSystem().Death -= EnableDeathScreen;
+        }
+
+        private void EnableDeathScreen()
+        {
+            DeathScreen.SetActive(true);
+        }
     }
 }
