35ccf06c660529f20cdd1abaa5970a205b9b98ef
Lucas Postai <lucasspostai@live.com>
2020-06-27 16:22:06 -0300

    feat: add enemy AI triggers

--- Assets/Scripts/Framework/LevelDesignEvents/ChangeLevelTrigger.cs
+++ Assets/Scripts/Framework/LevelDesignEvents/ChangeLevelTrigger.cs
@@ -7,12 +7,20 @@ namespace Framework.LevelDesignEvents
     public class ChangeLevelTrigger : LevelDesignEvent
     {
         [SerializeField] private Level NextLevel;
-        
+        [SerializeField] private StageArrivalLocation ArrivalLocation;
+
         public override void Execute()
         {
             base.Execute();
-            
+
             LevelLoader.Instance.LoadLevel(NextLevel);
         }
+    }
+
+    public enum StageArrivalLocation
+    {
+        Entrance,
+        Exit,
+        Shrine
     }
 }
