6ac868e1ad9a6386757a4efe171b5b93f0027adf
Lucas Postai <lucasspostai@live.com>
2020-06-27 00:57:03 -0300

    fix: Hiroyuki water reflections

---
+++ Assets/WaterReflex/Water2D.cs
@@ -0,0 +1,28 @@
+﻿// 2016 - Damien Mayance (@Valryon)
+// Source: https://github.com/valryon/water2d-unity/
+
+using UnityEngine;
+
+namespace WaterReflex
+{
+    public class Water2D : MonoBehaviour
+    {
+        [SerializeField] private Vector2 Speed = new Vector2(0.01f, 0f);
+
+        private Renderer waterRenderer;
+        private Material waterMaterial;
+
+        private void Awake()
+        {
+            waterRenderer = GetComponent<Renderer>();
+            waterMaterial = waterRenderer.material;
+        }
+
+        private void LateUpdate()
+        {
+            var scroll = Time.deltaTime * Speed;
+
+            waterMaterial.mainTextureOffset += scroll;
+        }
+    }
+}
---
+++ Assets/WaterReflex/WaterReflection.cs
@@ -0,0 +1,70 @@
+﻿// 2016 - Damien Mayance (@Valryon)
+// Source: https://github.com/valryon/water2d-unity/
+
+using UnityEngine;
+
+namespace WaterReflex
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class WaterReflection : MonoBehaviour
+    {
+        #region Members
+
+        private SpriteRenderer spriteSource;
+        private SpriteRenderer spriteRenderer;
+
+        [Header("Reflection Properties")] 
+        [SerializeField] private Vector3 LocalPosition = new Vector3(0, -0.25f, 0);
+        [SerializeField] private Vector3 LocalRotation = new Vector3(0, 0, -180);
+        [SerializeField] private Vector3 LocalScale = Vector3.one;
+
+        [Tooltip("Optional: force the reflected sprite. If null it will be a copy of the source.")]
+        [SerializeField] private Sprite Sprite;
+        [SerializeField] private string SpriteLayer = "Default";
+        [SerializeField] private int SpriteLayerOrder = -5;
+        private bool isSpriteNull;
+        private bool isSpriteSourceNotNull;
+
+        #endregion
+
+        #region Timeline
+
+        private void Awake()
+        {
+            isSpriteSourceNotNull = spriteSource != null;
+            isSpriteNull = Sprite == null;
+            
+            var reflectGameObject = new GameObject("Water Reflect");
+            reflectGameObject.transform.parent = transform;
+            reflectGameObject.transform.localPosition = LocalPosition;
+            reflectGameObject.transform.localRotation = Quaternion.Euler(LocalRotation);
+            reflectGameObject.transform.localScale = LocalScale;
+
+            spriteRenderer = reflectGameObject.AddComponent<SpriteRenderer>();
+            spriteRenderer.sortingLayerName = SpriteLayer;
+            spriteRenderer.sortingOrder = SpriteLayerOrder;
+
+            spriteSource = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnDestroy()
+        {
+            if (spriteRenderer != null)
+            {
+                Destroy(spriteRenderer.gameObject);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (!isSpriteSourceNotNull) return;
+            spriteRenderer.sprite = isSpriteNull ? spriteSource.sprite : Sprite;
+
+            spriteRenderer.flipX = spriteSource.flipX;
+            spriteRenderer.flipY = spriteSource.flipY;
+            spriteRenderer.color = spriteSource.color;
+        }
+
+        #endregion
+    }
+}
